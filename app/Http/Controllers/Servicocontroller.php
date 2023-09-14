61e7aa14dfda9f6a78ccfefcc4285fadeff16fa0
EmyyS2 <miacontapcsenai@gmail.com>
2023-09-14 14:17:43 -0300

    servico

--- app/Http/Controllers/Servicocontroller.php
+++ app/Http/Controllers/Servicocontroller.php
@@ -8,22 +8,21 @@ use Illuminate\Http\Request;
 
 class ServicoController extends Controller
 {
-    
+
     public function Servico(ServicoFormRequest $request)
     {
         $servico = Servico::create([
             'nome' => $request->nome,
-            'descricao'=>$request->descricao,
-            'duracao'=>$request->duracao,
-            'preco'=>$request->preco,
+            'descricao' => $request->descricao,
+            'duracao' => $request->duracao,
+            'preco' => $request->preco,
 
         ]);
         return response()->json([
-            "sucess"=>true,
-            "message"=> "Servico Cadastrado com sucesso",
-        "data"=> $servico
+            "sucess" => true,
+            "message" => "Servico Cadastrado com sucesso",
+            "data" => $servico
         ]);
-
     }
     public function pesquisarPorNome(Request $request)
     {
@@ -72,43 +71,44 @@ class ServicoController extends Controller
             ]);
         }
 
-$servico->delete();
+        $servico->delete();
 
-return response()->json([
-    'status'=>true,
-    'message'=> "serviço excluído com sucesso"
-]);
-
+        return response()->json([
+            'status' => true,
+            'message' => "serviço excluído com sucesso"
+        ]);
     }
 
-public function update(Request $request){
-$servico= Servico::find($request->id);
-if(!isset($servico)){
-    return response()->json([
-        'status' => false,
-        'message' => "servico não encontrado"
-    ]);
-    if(!isset($request->nome)){
-        $servico->nome = $request->nome;
+    public function update(Request $request)
+    {
+        $servico = Servico::find($request->id);
+        if (!isset($servico)) {
+            return response()->json([
+                'status' => false,
+                'message' => "servico não encontrado"
+            ]);
+        
+        }
+        if (isset($request->nome)) {
+            $servico->nome = $request->nome;
+        }
+
+        if (isset($request->descricao)) {
+            $servico->descricao = $request->descricao;
+        }
+
+        if (isset($request->duracao)) {
+            $servico->duracao = $request->duracao;
+        }
+
+        if (isset($request->preco)) {
+            $servico->preco = $request->preco;
+        }
+        $servico->update();
+
+        return response()->json([
+            'status' => true,
+            'message' => "servico atualizado"
+        ]);
     }
-    if(!isset($request->descricao)){
-$servico->descricao = $request->descricao;
-    }
-
-    if(!isset($request->preco)){
-$servico->preco = $request->preco;
-    }
-
-
-
-}
-$servico->update();
-
-return response()->json([
-    'status'=>true,
-    'message'=> "servico atualizado"
-]);
-
-}
-
 }
