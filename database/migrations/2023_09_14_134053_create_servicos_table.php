79df9e47b3a495452b08863cfe3623ee994dfd94
EmyyS2 <miacontapcsenai@gmail.com>
2023-09-14 13:12:07 -0300

    servico

--- database/migrations/2023_09_14_134053_create_servicos_table.php
+++ database/migrations/2023_09_14_134053_create_servicos_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('servicos', function (Blueprint $table) {
+        Schema::create('servico', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('nome',80)->unique()->nullable(false);
